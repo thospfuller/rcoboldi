600467995356399be4d8c9397d7e6e422bfb6717
Thomas P. Fuller <thomas.fuller@coherentlogic.com>
2020-04-17 14:21:38 -0400

    Added a few notes.

--- docker-build-rstudio.sh
+++ docker-build-rstudio.sh
@@ -1,1 +1,3 @@
 docker build docker/rocker/rstudio --tag rcoboldi/rocker-rstudio:1.0.2
+echo docker push thospfuller/rcoboldi-rocker-rstudio
+echo docker pull thospfuller/rcoboldi-rocker-rstudio:1.0.2

85f2c90475dc1ce87271a441d7b6299357cf0ed1
Thomas P. Fuller <thomas.fuller@coherentlogic.com>
2020-04-20 19:43:33 -0400

    New exception added.

---
+++ java/rcoboldi-core/src/main/java/com/coherentlogic/rproject/integration/rcoboldi/api/UnrecognizedFileStructureRuntimeException.java
@@ -0,0 +1,8 @@
+package com.coherentlogic.rproject.integration.rcoboldi.api;
+
+public class UnrecognizedFileStructureRuntimeException extends RuntimeException {
+
+    public UnrecognizedFileStructureRuntimeException(String message) {
+        super(message);
+    }
+}

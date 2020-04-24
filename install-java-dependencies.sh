9988fc26956485bfd262231d92efd20f7e188f86
Thomas P. Fuller <thomas.fuller@coherentlogic.com>
2020-04-24 12:32:56 -0400

    Sorting out Travis CI build issues.

--- install-java-dependencies.sh
+++ install-java-dependencies.sh
@@ -9,12 +9,14 @@ git clone https://bitbucket.org/CoherentLogic/jdataframe.git ~/temp/jdataframe
 # The TLSv1.2 is here to fix a 'peer not authenticated' error which causes the build to fail
 # when attempting to download dependencies from FuseSource.
 #
-cd ~/temp/eda/ && mvn clean install -DskipTests=true -U -Dhttps.protocols=TLSv1.2
-cd ~/temp/jdataframe/ && mvn clean install -DskipTests=true -U
+bash cd ~/temp/eda/ && mvn clean install -DskipTests=true -U -Dhttps.protocols=TLSv1.2
+bash cd ~/temp/jdataframe/ && mvn clean install -DskipTests=true -U
+
+# cd /home/travis/build/thospfuller/rcoboldi
 
 wget https://sourceforge.net/projects/coboltocsv/files/CobolToCsv/Version_0.90/Cobol2Csv_0.90.zip -P ~/temp/ -O Cobol2Csv_0.90.zip
 wget https://sourceforge.net/projects/jrecord/files/jrecord/Version_0.90.3/JRecord_Version_0.90.3.zip -P ~/temp/ -O JRecord_Version_0.90.3.zip
-unzip ~/temp/Cobol2Csv_0.90.zip -d ~/temp/Cobol2Csv/
+unzip /home/travis/build/thospfuller/temp/Cobol2Csv_0.90.zip -d ~/temp/Cobol2Csv/
 unzip ~/temp/JRecord_Version_0.90.3.zip -d ~/temp/jrecord
 
 mvn install:install-file -Dfile=~/temp/jrecord/lib/JRecord.jar -DgroupId=net.sf -DartifactId=jrecord -Dversion=0.90.2 -Dpackaging=jar

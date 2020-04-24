3133214773a91899ec366a5a3b7e5343158185c6
Thomas P. Fuller <thomas.fuller@coherentlogic.com>
2020-04-24 12:09:02 -0400

    Sorting out Travis CI build issues.

--- install-java-dependencies.sh
+++ install-java-dependencies.sh
@@ -12,18 +12,12 @@ git clone https://bitbucket.org/CoherentLogic/jdataframe.git ~/temp/jdataframe
 cd ~/temp/eda/ && mvn clean install -DskipTests=true -U -Dhttps.protocols=TLSv1.2
 cd ~/temp/jdataframe/ && mvn clean install -DskipTests=true -U
 
-wget https://sourceforge.net/projects/coboltocsv/files/CobolToCsv/Version_0.90/Cobol2Csv_0.90.zip -P ~/temp/
-wget https://sourceforge.net/projects/jrecord/files/jrecord/Version_0.90.3/JRecord_Version_0.90.3.zip -P ~/temp/
+wget https://sourceforge.net/projects/coboltocsv/files/CobolToCsv/Version_0.90/Cobol2Csv_0.90.zip -P ~/temp/ -O Cobol2Csv_0.90.zip
+wget https://sourceforge.net/projects/jrecord/files/jrecord/Version_0.90.3/JRecord_Version_0.90.3.zip -P ~/temp/ -O JRecord_Version_0.90.3.zip
 unzip ./temp/Cobol2Csv_0.90.zip -d ~/temp/Cobol2Csv/
 unzip ./temp/JRecord_Version_0.90.3.zip -d ~/temp/jrecord
-echo "installing"
 ls -la ~/temp/
-echo =====>
-ls -la ~/temp/Cobol2Csv/
-echo =====>
-ls -la ~/temp/jrecord
-echo Now installing...
+
 mvn install:install-file -Dfile=~/temp/jrecord/lib/JRecord.jar -DgroupId=net.sf -DartifactId=jrecord -Dversion=0.90.2 -Dpackaging=jar
 mvn install:install-file -Dfile=~/temp/jrecord/lib/cb2xml.jar -DgroupId=net.sf -DartifactId=cb2xml -Dversion=0.90.2 -Dpackaging=jar
 mvn install:install-file -Dfile=~/temp/Cobol2Csv/lib/Cobol2Csv.jar -DgroupId=net.sf -DartifactId=cb2csv -Dversion=0.90 -Dpackaging=jar
-echo Done!

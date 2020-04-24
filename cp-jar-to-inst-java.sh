588f0b87103bb67f2621bb636dccb8178e58122d
Thomas P. Fuller <thomas.fuller@coherentlogic.com>
2020-04-24 16:48:23 -0400

    Fixing more issues.

--- cp-jar-to-inst-java.sh
+++ cp-jar-to-inst-java.sh
@@ -1,1 +1,1 @@
-cp $HOME/build/thospfuller/rcoboldi/java/rcoboldi-assembly/target/rcoboldi-assembly-*-RELEASE.jar $HOME/build/thospfuller/rcoboldi/r-package/inst/java
+cp $HOME/build/thospfuller/rcoboldi/java/rcoboldi-assembly/target/rcoboldi-assembly-1.0.2-RELEASE.jar $HOME/build/thospfuller/rcoboldi/r-package/inst/java

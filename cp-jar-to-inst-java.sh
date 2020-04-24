55ed37f6399b369c5bc05c092c4774b3175d92d8
Thomas P. Fuller <thomas.fuller@coherentlogic.com>
2020-04-24 14:22:36 -0400

    Replaced ll with ls -la.

--- cp-jar-to-inst-java.sh
+++ cp-jar-to-inst-java.sh
@@ -1,2 +1,2 @@
-ll ./java/rcoboldi-assembly/target/rcoboldi-assembly-*-RELEASE.jar
+ls -la ./java/rcoboldi-assembly/target/rcoboldi-assembly-*-RELEASE.jar
 cp ./java/rcoboldi-assembly/target/rcoboldi-assembly-*-RELEASE.jar ./r-package/inst/java

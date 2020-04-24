bb8bd4875cff2578036b4f4e90405d5cac2c5306
Thomas P. Fuller <thomas.fuller@coherentlogic.com>
2020-04-24 12:46:34 -0400

    Sorting out Travis CI build issues.

--- install-java-dependencies.sh
+++ install-java-dependencies.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
-pwd
-mkdir ~/temp
+# pwd = /home/travis/build/thospfuller/rcoboldi
 
-git clone https://bitbucket.org/CoherentLogic/coherent-logic-enterprise-data-adapter.git ~/temp/eda
-git clone https://bitbucket.org/CoherentLogic/jdataframe.git ~/temp/jdataframe
+mkdir $HOME/temp
+
+git clone https://bitbucket.org/CoherentLogic/coherent-logic-enterprise-data-adapter.git $HOME/temp/eda
+git clone https://bitbucket.org/CoherentLogic/jdataframe.git $HOME/temp/jdataframe
 
 #
 # The TLSv1.2 is here to fix a 'peer not authenticated' error which causes the build to fail
 # when attempting to download dependencies from FuseSource.
 #
-bash cd ~/temp/eda/ && mvn clean install -DskipTests=true -U -Dhttps.protocols=TLSv1.2
-bash cd ~/temp/jdataframe/ && mvn clean install -DskipTests=true -U
+cd $HOME/temp/eda/ && mvn clean install -DskipTests=true -U -Dhttps.protocols=TLSv1.2
+cd $HOME/temp/jdataframe/ && mvn clean install -DskipTests=true -U
 
-# cd /home/travis/build/thospfuller/rcoboldi
+cd /home/travis/build/thospfuller/rcoboldi
 
-wget https://sourceforge.net/projects/coboltocsv/files/CobolToCsv/Version_0.90/Cobol2Csv_0.90.zip -P ~/temp/ -O Cobol2Csv_0.90.zip
-wget https://sourceforge.net/projects/jrecord/files/jrecord/Version_0.90.3/JRecord_Version_0.90.3.zip -P ~/temp/ -O JRecord_Version_0.90.3.zip
-unzip /home/travis/build/thospfuller/temp/Cobol2Csv_0.90.zip -d ~/temp/Cobol2Csv/
-unzip ~/temp/JRecord_Version_0.90.3.zip -d ~/temp/jrecord
+wget https://sourceforge.net/projects/coboltocsv/files/CobolToCsv/Version_0.90/Cobol2Csv_0.90.zip -P $HOME/temp/ -O Cobol2Csv_0.90.zip
+wget https://sourceforge.net/projects/jrecord/files/jrecord/Version_0.90.3/JRecord_Version_0.90.3.zip -P $HOME/temp/ -O JRecord_Version_0.90.3.zip
+unzip $HOME/temp/Cobol2Csv_0.90.zip -d $HOME/temp/Cobol2Csv/
+unzip $HOME/temp/JRecord_Version_0.90.3.zip -d $HOME/temp/jrecord
 
-mvn install:install-file -Dfile=~/temp/jrecord/lib/JRecord.jar -DgroupId=net.sf -DartifactId=jrecord -Dversion=0.90.2 -Dpackaging=jar
-mvn install:install-file -Dfile=~/temp/jrecord/lib/cb2xml.jar -DgroupId=net.sf -DartifactId=cb2xml -Dversion=0.90.2 -Dpackaging=jar
-mvn install:install-file -Dfile=~/temp/Cobol2Csv/lib/Cobol2Csv.jar -DgroupId=net.sf -DartifactId=cb2csv -Dversion=0.90 -Dpackaging=jar
+mvn install:install-file -Dfile=$HOME/temp/jrecord/lib/JRecord.jar -DgroupId=net.sf -DartifactId=jrecord -Dversion=0.90.2 -Dpackaging=jar
+mvn install:install-file -Dfile=$HOME/temp/jrecord/lib/cb2xml.jar -DgroupId=net.sf -DartifactId=cb2xml -Dversion=0.90.2 -Dpackaging=jar
+mvn install:install-file -Dfile=$HOME/temp/Cobol2Csv/lib/Cobol2Csv.jar -DgroupId=net.sf -DartifactId=cb2csv -Dversion=0.90 -Dpackaging=jar

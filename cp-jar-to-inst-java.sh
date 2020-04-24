0ddfca52b8886e62ef676bb995138c710fa5c976
Thomas P. Fuller <thomas.fuller@coherentlogic.com>
2020-04-24 18:27:37 -0400

    More changes to get this Travis CI build working.

--- cp-jar-to-inst-java.sh
+++ cp-jar-to-inst-java.sh
@@ -1,2 +1,4 @@
 ls -la $HOME/build/thospfuller/rcoboldi/java/rcoboldi-assembly/target/
+mkdir $HOME/build/thospfuller/rcoboldi/r-package/inst/
+mkdir $HOME/build/thospfuller/rcoboldi/r-package/inst/java
 cp $HOME/build/thospfuller/rcoboldi/java/rcoboldi-assembly/target/rcoboldi-assembly-1.0.2-RELEASE.jar $HOME/build/thospfuller/rcoboldi/r-package/inst/java

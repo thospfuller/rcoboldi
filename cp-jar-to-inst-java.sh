a5d447e424cbe0d98bc1d25474ad81e0c48a400c
Thomas P. Fuller <thomas.fuller@coherentlogic.com>
2020-04-24 14:14:39 -0400

    Sorting out Travis CI R-related build issues.

--- cp-jar-to-inst-java.sh
+++ cp-jar-to-inst-java.sh
@@ -1,1 +1,2 @@
-cp java/rcoboldi-assembly/target/rcoboldi-assembly-1.0.2-RELEASE.jar r-package/inst/java
+ll ./java/rcoboldi-assembly/target/rcoboldi-assembly-*-RELEASE.jar
+cp ./java/rcoboldi-assembly/target/rcoboldi-assembly-*-RELEASE.jar ./r-package/inst/java

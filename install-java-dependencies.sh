3ed0759892d9c6578e40c005ea40fe1c77ee42e2
Thomas P. Fuller <thomas.fuller@coherentlogic.com>
2020-04-24 13:37:40 -0400

    Sorting out Travis CI build issues.

--- install-java-dependencies.sh
+++ install-java-dependencies.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # pwd = /home/travis/build/thospfuller/rcoboldi
 
 mkdir $HOME/temp
@@ -13,12 +14,12 @@ git clone https://bitbucket.org/CoherentLogic/jdataframe.git $HOME/temp/jdatafra
 cd $HOME/temp/eda/ && mvn clean install -DskipTests=true -U -Dhttps.protocols=TLSv1.2
 cd $HOME/temp/jdataframe/ && mvn clean install -DskipTests=true -U
 
-cd /home/travis/build/thospfuller/rcoboldi
+cd $HOME
 
 wget https://sourceforge.net/projects/coboltocsv/files/CobolToCsv/Version_0.90/Cobol2Csv_0.90.zip -O $HOME/temp/Cobol2Csv_0.90.zip
 wget https://sourceforge.net/projects/jrecord/files/jrecord/Version_0.90.3/JRecord_Version_0.90.3.zip -O $HOME/temp/JRecord_Version_0.90.3.zip
 unzip $HOME/temp/Cobol2Csv_0.90.zip -d $HOME/temp/
-unzip $HOME/temp/JRecord_Version_0.90.3.zip -d $HOME/temp/
+unzip $HOME/temp/JRecord_Version_0.90.3.zip -d $HOME/temp/jrecord/
 
 mvn install:install-file -Dfile=$HOME/temp/jrecord/lib/JRecord.jar -DgroupId=net.sf -DartifactId=jrecord -Dversion=0.90.2 -Dpackaging=jar
 mvn install:install-file -Dfile=$HOME/temp/jrecord/lib/cb2xml.jar -DgroupId=net.sf -DartifactId=cb2xml -Dversion=0.90.2 -Dpackaging=jar
